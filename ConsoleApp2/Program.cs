1a33cb744c69a9f21e3a641ea9148d79aa957035
radeen <radeen013@gmail.com>
2021-10-07 13:13:30 -0400

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Github merge example project
+        }
+
+        static bool person1(int num1, int num2)
+        {
+            
+        }
+
+        static bool person2(int num1, int num2)
+        {
+
+        }
+    }
+}
